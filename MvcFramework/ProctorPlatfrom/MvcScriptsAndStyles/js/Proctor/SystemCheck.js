7e9b992b91391974cedf909f86055d32515f3153
developer-kgetechnologies-HPSSD <developer@kgetechnologies.com>
2024-01-08 14:01:13 +0530

    js

---
+++ MvcFramework/ProctorPlatfrom/MvcScriptsAndStyles/js/Proctor/SystemCheck.js
@@ -0,0 +1,146 @@
+﻿var imageAddr = "https://www.brainmeasures.com/images/022.jpg";
+var downloadSize = 1995374; //bytes
+
+function InitiateSpeedDetection() {
+    window.setTimeout(MeasureConnectionSpeed, 1);
+};
+
+if (window.addEventListener) {
+    window.addEventListener('load', InitiateSpeedDetection, false);
+} else if (window.attachEvent) {
+    window.attachEvent('onload', InitiateSpeedDetection);
+}
+
+function MeasureConnectionSpeed() {
+    var startTime, endTime;
+    var download = new Image();
+    download.onload = function () {
+        endTime = (new Date()).getTime();
+        showResults();
+    }
+
+    function showResults() {
+        var duration = (endTime - startTime) / 1000;
+        var bitsLoaded = downloadSize * 8;
+        var speedBps = (bitsLoaded / duration).toFixed(2);
+        var speedKbps = (speedBps / 1024).toFixed(2);
+        var speedMbps = (speedKbps / 1024).toFixed(2);
+
+        if (speedMbps >= 1.0) {
+            $('#speed-error').addClass('d-none');
+            $('#speed-success').removeClass('d-none');
+            if (Isvalid) {
+                $('#go-button').removeAttr('disabled');
+            }
+        }
+        else {
+            $('#speed-error').removeClass('d-none');
+            $('#speed-success').addClass('d-none');
+        }
+    }
+
+    download.onerror = function (err, msg) {
+        $('#speed-error').removeClass('d-none');
+        $('#speed-success').addClass('d-none');
+    }
+
+    startTime = (new Date()).getTime();
+    var cacheBuster = "?nnn=" + startTime;
+    download.src = imageAddr + cacheBuster;
+}
+
+var Isvalid = true;
+var imgwebrtc = true;
+var Isflash = true;
+function onDetectRTCLoaded() {
+    var name = DetectRTC.osName;
+    //alert("OS : " + name);
+    if (name == 'Windows' || name.toUpperCase().indexOf('MAC') >= 0) {
+        $('#os-error').addClass('d-none');
+        $('#os-success').removeClass('d-none');
+    }
+    else {
+        $('#os-error').removeClass('d-none');
+        $('#os-success').addClass('d-none');
+        Isvalid = false;
+    }
+    if (DetectRTC.browser != null && DetectRTC.browser.isChrome != null && DetectRTC.browser.isChrome) {
+        $('#browser-error').addClass('d-none');
+        $('#browser-success').removeClass('d-none');
+    }
+    else {
+        $('#browser-error').removeClass('d-none');
+        $('#browser-success').addClass('d-none');
+        Isvalid = false;
+    }
+
+    if (DetectRTC.hasMicrophone) {
+        $('#mic-error').addClass('d-none');
+        $('#mic-success').removeClass('d-none');
+    }
+    else {
+        $('#mic-error').removeClass('d-none');
+        $('#mic-success').addClass('d-none');
+        Isvalid = false;
+    }
+
+    if (DetectRTC.hasSpeakers) {
+        $('#speaker-error').addClass('d-none');
+        $('#speaker-success').removeClass('d-none');
+    }
+    else {
+        $('#speaker-error').removeClass('d-none');
+        $('#speaker-success').addClass('d-none');
+        Isvalid = false;
+    }
+
+    if (DetectRTC.isWebRTCSupported) {
+    }
+    else {
+        Isvalid = false;
+
+        if (imgwebrtc == true) {
+            imgwebrtc == false;
+        }
+    }
+
+    if (DetectRTC.hasWebcam) {
+        $('#cam-error').addClass('d-none');
+        $('#cam-success').removeClass('d-none');
+    }
+    else {
+        $('#cam-error').removeClass('d-none');
+        $('#cam-success').addClass('d-none');
+        Isvalid = false;
+    }
+    if (DetectRTC.isWebSocketsSupported) {
+    }
+    else {
+        Isvalid = false;
+    }
+    if ($(".bg-danger.d-none")?.length == 6) {
+        $(".step-next-3")?.show();
+    }
+    else {
+        $(".step-next-3")?.hide();
+    }
+}
+
+function reloadDetectRTC() {
+    DetectRTC.load(onDetectRTCLoaded);
+}
+
+DetectRTC.load(function () {
+    reloadDetectRTC();
+
+    if (DetectRTC.MediaDevices[0] && DetectRTC.MediaDevices[0].label === 'Please invoke getUserMedia once.') {
+        navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(reloadDetectRTC).catch(reloadDetectRTC);
+        return;
+    }
+
+    onDetectRTCLoaded();
+});
+
+$(document).ready(function () {
+    reloadDetectRTC();
+});
---
+++ MvcFramework/ProctorPlatfrom/MvcScriptsAndStyles/js/Profile/Profile.js
@@ -0,0 +1,55 @@
+﻿
+
+function UpdateSecretQuestion() {
+  
+    if (ValidateSecurityQuestionInputs()) {
+        var SecurityQueAnsDetail = [];
+        $("#dvInputControls .spnInputControls").each(function () {
+            var collectInputDetail = {};
+            collectInputDetail.QuestionId = $($(this).find('.qns')[0]).attr("data-qid");
+            collectInputDetail.UserAnswer = $(this).find('input.txtanswer').val();
+            SecurityQueAnsDetail.push(collectInputDetail);
+        });
+        $('#submit-sec-qns').val($('#submit-sec-qns').data("loading-text")); 
+        $.ajax({
+            url: "/Test/UpdateSecurityQuestion",
+            type: "POST",
+            data: { 'questionInput': SecurityQueAnsDetail },
+            success: function (result) {
+                   window.SecQnsCallBack && window.SecQnsCallBack(result);
+             
+            }
+        });
+        return false;
+    }
+    return false;
+}
+
+function ValidateSecurityQuestionInputs() {
+    var validateMessage = '<ul>';
+    var isValid = true;
+   // var isQuestionSelect = true;
+    var isAnsEnter = true;
+
+
+    $('#dvInputControls .spnInputControls').each(function (i, selected) {
+     
+        if ($(this).find('input.txtanswer').val() == '') {
+            isAnsEnter = false;
+
+        }
+
+    });   
+
+    if (!isAnsEnter) {
+        validateMessage += '<li>Please enter answers for all three security questions.</li>';
+        isValid = false;
+    }
+
+    if (!isValid) {
+        validateMessage += '</ul>';
+        alert(validateMessage);
+    }
+    return isValid;
+}
+
